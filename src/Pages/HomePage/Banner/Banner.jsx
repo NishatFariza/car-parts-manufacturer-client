ba4f5b6de24cfa607fbcac4bbb43fcd8ae3608e9
NishatFariza <nishatfarzia78@gmail.com>
2022-05-22 22:05:04 +0600

    banner component added

--- src/Pages/HomePage/Banner/Banner.jsx
+++ src/Pages/HomePage/Banner/Banner.jsx
@@ -2,8 +2,18 @@ import React from "react";
 
 const Banner = () => {
   return (
-    <div>
-      <h1>This is Home</h1>
+    <div className="bg-[url('https://i.ibb.co/jLsYZ0f/banner.png')] bg-cover">
+      <div className="md:h-[90vh] h-[80vh] flex flex-col justify-center items-start">
+        <div className="pl-10">
+          <h4 className="primary-color lg:text-3xl md:text-2xl text-xl ">
+            NEW TECHNOLOGY & BUILD
+          </h4>
+          <h1 className="md:text-7xl text-2xl font-bold primary-color">
+            WHEELS & TIRES <br /> COLLECTIONS
+          </h1>
+          <button className="btn glass">Shop NOW</button>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Pages/HomePage/Home/Home.jsx
+++ src/Pages/HomePage/Home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Banner from '../Banner/Banner';
 
 const Home = () => {
     return (
         <div>
-            
+            <Banner></Banner>
         </div>
     );
 };
