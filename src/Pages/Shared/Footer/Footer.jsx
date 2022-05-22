3be379341b40bf6bf3205f5a1f56e2f7c2037cf9
NishatFariza <nishatfarzia78@gmail.com>
2022-05-22 20:23:21 +0600

    footer text added

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -1,17 +1,36 @@
 import React from "react";
 import "./Footer.css";
 import { AiFillHeart } from "react-icons/ai";
+import { Link } from "react-router-dom";
+import footerLogo from "../../../images/logoFooter .png";
 
 const Footer = () => {
   const today = new Date();
   const year = today.getFullYear();
   return (
-    <div className="py-8 footer-bg text-white">
-      <div className="w-11/12 text-center mx-auto">
-        <small className="footer-text sm:text-sm text-xs">
-          Copyright &copy; {year}. Made With by <AiFillHeart className="text-red-700"></AiFillHeart>{" "}
-          Nishat Fariza
-        </small>
+    <div className="  text-white">
+      <div className="lg:py-20 py-16 footer-bg grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 lg:px-6 px-3">
+        <div className="">
+          <Link to="/">
+            <img className="lg:w-6/12 mb-6" src={footerLogo} alt="" />
+          </Link>
+          <p className="footer-description">
+            The economic impact of machinery manufacturing extends throughout
+            the U.S. economy. Machinery industries provide essential and highly.
+          </p>
+        </div>
+        <div>
+          <h4 className="text-xl font-semibold underline">Information</h4>
+        </div>
+      </div>
+      <div className="bg-black">
+        <div className="w-11/12 text-center mx-auto py-8">
+          <small className="footer-text sm:text-sm text-xs">
+            &copy; {year}.Facmaster, All Rights Reserved. Made With by
+            <AiFillHeart className="text-red-700 inline-block mx-1"></AiFillHeart>
+            Nishat Fariza
+          </small>
+        </div>
       </div>
     </div>
   );
