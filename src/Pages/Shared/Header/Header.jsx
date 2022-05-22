07e7b43b3ea4f4318c583a3231c7afaf6e7a84fc
NishatFariza <nishatfarzia78@gmail.com>
2022-05-22 19:01:35 +0600

    navbar added

--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -1,12 +1,66 @@
 import React from "react";
+import logo from "../../../images/logo.png";
 
 const Header = () => {
   return (
     <div class="navbar bg-base-100">
-      <div class="flex-1">
-        <a class="btn btn-ghost normal-case text-xl">daisyUI</a>
+      <div class="navbar-start">
+        <div class="dropdown">
+          <label tabindex="0" class="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabindex="0"
+            class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <a>Item 1</a>
+            </li>
+            <li tabindex="0">
+              <a class="justify-between">
+                Parent
+                <svg
+                  class="fill-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
+                </svg>
+              </a>
+              <ul class="p-2">
+                <li>
+                  <a>Submenu 1</a>
+                </li>
+                <li>
+                  <a>Submenu 2</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a>Item 3</a>
+            </li>
+          </ul>
+        </div>
+        <a class="lg:w-4/12 w-2/12 normal-case text-xl">
+          <img src={logo} alt="" />
+        </a>
       </div>
-      <div class="flex-none">
+      <div class="navbar-center hidden lg:flex">
         <ul class="menu menu-horizontal p-0">
           <li>
             <a>Item 1</a>
@@ -24,7 +78,7 @@ const Header = () => {
                 <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
               </svg>
             </a>
-            <ul class="p-2 bg-base-100">
+            <ul class="p-2">
               <li>
                 <a>Submenu 1</a>
               </li>
