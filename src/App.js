399873319ce2acc545bfa656f5f76310159a60ce
NishatFariza <nishatfarzia78@gmail.com>
2022-05-25 04:47:01 +0600

    notfound component implement

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Register from './Pages/Auth/Register/Register';
 import Home from './Pages/HomePage/Home/Home';
 import Footer from './Pages/Shared/Footer/Footer';
 import Header from './Pages/Shared/Header/Header';
+import NotFound from './Pages/Shared/NotFound/NotFound';
+
 
 function App() {
   return (
@@ -15,6 +17,7 @@ function App() {
        <Route path='/' element={<Home></Home>}></Route>
        <Route path='/login' element={<LogIn></LogIn>}></Route>
        <Route path='/register' element={<Register></Register>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
      </Routes>
      <Footer></Footer>
     </div>
--- src/Pages/Shared/NotFound/NotFound.jsx
+++ src/Pages/Shared/NotFound/NotFound.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./NotFound.css";
 
 const NotFound = () => {
-    return (
-        <div>
-            
+  return (
+    <div className="lg:h-[90vh] py-16 mx-auto notFound-bg flex justify-center items-center">
+      <div className="lg:w-8/12 w-11/12 mx-auto ">
+        <div className="text-center ">
+          <p className="sm:text-9xl text-xl font-bold text-white">404</p>
+          <p className="sm:text-4xl text-xl font-bold text-white">
+            File Not Found
+          </p>
+          <Link className="btn btn-outline text-white mt-4" to="/">
+            Go Back Home
+          </Link>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
