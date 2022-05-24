bb14da27996d8b911bbb776b53c01c0427cea7bd
NishatFariza <nishatfarzia78@gmail.com>
2022-05-24 12:17:12 +0600

    product data fetch

--- src/Pages/HomePage/AllProducts/Products/Products.jsx
+++ src/Pages/HomePage/AllProducts/Products/Products.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const Products = () => {
-    return (
-        <div>
-            <h2>This is Products</h2>
-        </div>
-    );
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const url = `http://localhost:5000/products`;
+    (async () => {
+      const { data } = await axios.get(url);
+      setProducts(data);
+    //   console.log(data);
+    })();
+  }, []);
+  return (
+    <div>
+      <div className="lg:w-6/12 md:w-8/12 sm:w-10/12 w-11/12 mx-auto text-center my-8">
+        <h2 className="md:text-4xl text-2xl font-semibold mb-2">
+          OUR PRODUCTS
+        </h2>
+        <div className="h-1 w-24 rounded-lg bg-yellow-400 mx-auto"></div>
+        <p className="mt-3">
+          All best seller product are now available for you and your can buy
+          this product from here any time any where so sop now
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Products;
