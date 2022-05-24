0840cd8aafe41bed8cc4ce583079afc9ad97fe6b
NishatFariza <nishatfarzia78@gmail.com>
2022-05-24 20:33:15 +0600

    review backgroud image add

--- src/Pages/HomePage/AllReviews/Reviews/Reviews.jsx
+++ src/Pages/HomePage/AllReviews/Reviews/Reviews.jsx
@@ -15,14 +15,14 @@ const Reviews = () => {
     return <p>Loading....</p>;
   }
   return (
-    <div>
+    <div className="bg-[url('https://i.ibb.co/5WmVLKK/review-bg.webp')] bg-cover">
       <div className="lg:w-6/12 md:w-8/12 sm:w-10/12 w-11/12 mx-auto text-center my-16">
         <h2 className="md:text-4xl text-2xl font-semibold mb-2 text-black">
           OUR PRODUCTS
         </h2>
         <div className="h-1 w-24 rounded-lg bg-yellow-600 mx-auto"></div>
       </div>
-      <div className="grid md:grid-cols-3 grid-cols-1 gap-7 pt-8 pb-16">
+      <div className="grid md:grid-cols-3 grid-cols-1 gap-7 pt-4 pb-16">
         {reviews.map((review) => (
           <Review reviewUser={review} key={review._id}></Review>
         ))}
