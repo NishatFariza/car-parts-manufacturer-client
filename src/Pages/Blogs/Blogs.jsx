12c2ae908312461522d7890564d836fbde193c3b
NishatFariza <nishatfarzia78@gmail.com>
2022-05-25 06:02:52 +0600

    blog text implement

--- src/Pages/Blogs/Blogs.jsx
+++ src/Pages/Blogs/Blogs.jsx
@@ -11,36 +11,31 @@ const Blogs = () => {
   return (
     <div>
       <div className="bg-[url('https://i.ibb.co/Qj8JDbk/page-header-bg.webp')] bg-cover md:h-[40vh] h-[40vh] flex flex-col justify-center items-center ">
-        <h3 className="text-white text-5xl">FAQ</h3>
+        <h3 className="text-white lg:text-5xl text-3xl">FAQ</h3>
       </div>
 
-      <div className="mx-auto sm:w-9/12 w-11/12 container sm:py-20 py-10">
+      <div className="mx-auto lg:w-9/12 w-11/12 container sm:py-20 py-10">
         <Accordion className="rounded duration-500 ">
           <AccordionItem className="box-shadow rounded duration-500">
-            <AccordionItemHeading className="border-t  sm:py-6 py-3 sm:px-3 px-0 rounded">
-              <AccordionItemButton className="sm:text-2xl text-xl font-bold text-yellow-500 flex items-start">
-                What is The Different Between JavaScript And NodeJS?
+            <AccordionItemHeading className="border-t  sm:py-6 py-3 sm:px-3 px-0 rounded hover:text-yellow-500 text-black cursor-pointer">
+              <AccordionItemButton className="lg:text-xl text-sm font-bold flex items-start duration-300">
+                How will you improve the performance of a React Application?
               </AccordionItemButton>
             </AccordionItemHeading>
             <AccordionItemPanel className="border-b">
               <p className="sm:px-6 px-3 pt-3 border-t sm:text-base text-sm">
                 <strong className="underline primary-color font-bold text-justify">
-                  JavaScript:
+                  Ja:
                 </strong>
-                Javascript is a programming language that is used for writing
-              </p>
-              <p className="sm:px-6 px-3 sm:text-base text-sm py-8">
-                <strong className="underline primary-color font-bold">
-                  NodeJS:
-                </strong>
-                NodeJS is a Javascript runtime environment. It is mostly used on
+                language that is used for writing
               </p>
             </AccordionItemPanel>
           </AccordionItem>
           <AccordionItem className="box-shadow border-y rounded duration-500">
-            <AccordionItemHeading className="border-y-b  py-6 sm:px-3 px-0 rounded">
-              <AccordionItemButton className="sm:text-2xl text-xl font-bold text-blue-400 flex items-center">
-                When Should We Use NodeJS? And When Should We Use MongoDB?
+            <AccordionItemHeading className="border-y-b  py-6 sm:px-3 px-0 rounded hover:text-yellow-500 text-black cursor-pointer">
+              <AccordionItemButton className="text-xl font-bold duration-300 flex items-start">
+                What are the different ways to manage a state in a React
+                application?
               </AccordionItemButton>
             </AccordionItemHeading>
             <AccordionItemPanel>
@@ -54,40 +49,12 @@ const Blogs = () => {
                   server in Javascript, then you can use nodejs for it.
                 </p>
               </p>
-              <p className="sm:px-6 px-3 sm:text-base text-sm py-8">
-                <strong className="underline primary-color font-bold">
-                  Reasons for using node MongoDB:
-                </strong>
-                (on AWS), Postgres are examples of other databases. <br />
-                <strong className="text-stone-600 mt-4">
-                  Here are the some main reasons for using MongoDB:
-                </strong>
-                <li>
-                  MongoDB is very flexible and adaptable to real business world
-                  situations and requirements.
-                </li>
-                <li>
-                  Queries can be made to return certain fields within documents.
-                </li>
-                <li>
-                  MongoDB supports field, range-based query, regular expression,
-                  etc. for searching the data from the stored data.
-                </li>
-                <li>
-                  The dynamic database schema used in MongoDB is called the
-                  JSON.
-                </li>
-                <li>
-                  MongoDB basically uses JavaScript objects in place of the
-                  procedure.
-                </li>
-              </p>
             </AccordionItemPanel>
           </AccordionItem>
           <AccordionItem className="box-shadow border-b rounded duration-500">
-            <AccordionItemHeading className="  py-6 sm:px-3 px-0 rounded">
-              <AccordionItemButton className="sm:text-2xl text-xl font-bold text-blue-400 flex items-center">
-                What is The Different Between SQL And NoSQL?
+            <AccordionItemHeading className="  py-6 sm:px-3 px-0 rounded hover:text-yellow-500 text-black cursor-pointer">
+              <AccordionItemButton className="text-xl font-bold duration-300 flex items-start">
+                How does prototypical inheritance work?
               </AccordionItemButton>
             </AccordionItemHeading>
             <AccordionItemPanel className="border-y ">
@@ -98,18 +65,48 @@ const Blogs = () => {
                 Also a lot of independent consultations are there who can help
                 you with SQL database for a very large scale deployments.
               </p>
-              <p className="sm:px-6 px-3 sm:text-base text-sm py-8">
-                <strong className="underline primary-color font-bold">
-                  NoSQL:
+            </AccordionItemPanel>
+          </AccordionItem>
+          <AccordionItem className="box-shadow border-b rounded duration-500">
+            <AccordionItemHeading className=" py-6 sm:px-3 px-0 rounded hover:text-yellow-500 text-black cursor-pointer">
+              <AccordionItemButton className="text-xl font-bold duration-300 flex items-start">
+                Why you do not set the state directly in React. For example, if
+                you have const [products, setProducts] = useState([]). Why you
+                do not set products = [...] instead, you use the setProducts?
+              </AccordionItemButton>
+            </AccordionItemHeading>
+            <AccordionItemPanel>
+              <p className="sm:px-6 px-3 sm:text-base text-sm pt-3 border-t">
+                <strong className="underline primary-color font-bold text-justify">
+                  JWT:
                 </strong>
-                for setting up and deploying your large scale NoSQL deployments.
+                the signature also certifies that only the party holding the
+                private key is the one that signed it.
               </p>
             </AccordionItemPanel>
           </AccordionItem>
           <AccordionItem className="box-shadow border-b rounded duration-500">
-            <AccordionItemHeading className=" py-6 sm:px-3 px-0 rounded">
-              <AccordionItemButton className="sm:text-2xl text-xl font-bold text-blue-400 flex items-center">
-                What is The Purpose of JWT? How dose Work JWT?
+            <AccordionItemHeading className=" py-6 sm:px-3 px-0 rounded hover:text-yellow-500 text-black cursor-pointer">
+              <AccordionItemButton className="text-xl font-bold duration-300 flex items-start">
+                You have an array of products. Each product has a name, price,
+                description, etc. How will you implement a search to find
+                products by name?
+              </AccordionItemButton>
+            </AccordionItemHeading>
+            <AccordionItemPanel>
+              <p className="sm:px-6 px-3 sm:text-base text-sm pt-3 border-t">
+                <strong className="underline primary-color font-bold text-justify">
+                  JWT:
+                </strong>
+                the signature also certifies that only the party holding the
+                private key is the one that signed it.
+              </p>
+            </AccordionItemPanel>
+          </AccordionItem>
+          <AccordionItem className="box-shadow border-b rounded duration-500">
+            <AccordionItemHeading className=" py-6 sm:px-3 px-0 rounded hover:text-yellow-500 text-black cursor-pointer">
+              <AccordionItemButton className="text-xl font-bold duration-300 flex items-start">
+                What is a unit test? Why should write unit tests?
               </AccordionItemButton>
             </AccordionItemHeading>
             <AccordionItemPanel>
