08c146b03086c86344b8bb1a2c4b523ba001ac80
NishatFariza <nishatfarzia78@gmail.com>
2022-05-26 05:50:02 +0600

    contact me component added

---
+++ src/Pages/MyPortfolioPage/ContactMe/ContactMe.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ContactMe = () => {
+  return (
+    <div>
+      <h3>Contact me</h3>
+    </div>
+  );
+};
+
+export default ContactMe;
--- src/Pages/MyPortfolioPage/MyPortfolio/MyPortfolio.jsx
+++ src/Pages/MyPortfolioPage/MyPortfolio/MyPortfolio.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
-import AboutMe from '../AboutMe/AboutMe';
-import MyPortfolioBanner from '../PortfolioBanner/PortfolioBanner';
+import React from "react";
+import AboutMe from "../AboutMe/AboutMe";
+import MySills from "../MySkills/MySills";
+import MyPortfolioBanner from "../PortfolioBanner/PortfolioBanner";
 
 const MyPortfolio = () => {
-    return (
-        <div>
-            <MyPortfolioBanner></MyPortfolioBanner>
-            <AboutMe></AboutMe>
-        </div>
-    );
+  return (
+    <div>
+      <MyPortfolioBanner></MyPortfolioBanner>
+      <AboutMe></AboutMe>
+      <MySills></MySills>
+      <MyPortfolio></MyPortfolio>
+    </div>
+  );
 };
 
 export default MyPortfolio;
