7119053e372b090bc11f0990501f8f365b94a039
NishatFariza <nishatfarzia78@gmail.com>
2022-05-26 05:47:34 +0600

    my project and skill component implement

---
+++ src/Pages/MyPortfolioPage/MyProjects/MyProjects.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyProjects = () => {
+    return (
+        <div>
+            <h4>My PROJECT</h4>
+        </div>
+    );
+};
+
+export default MyProjects;
---
+++ src/Pages/MyPortfolioPage/MySkills/MySills.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MySills = () => {
+  return (
+    <div>
+      <h3>My Skill</h3>
+    </div>
+  );
+};
+
+export default MySills;
