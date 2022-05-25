0a65351aef04b7a276da8cabed125afa4a6620b8
NishatFariza <nishatfarzia78@gmail.com>
2022-05-26 04:39:45 +0600

    about me image added

--- src/Pages/MyPortfolioPage/AboutMe/AboutMe.jsx
+++ src/Pages/MyPortfolioPage/AboutMe/AboutMe.jsx
@@ -1,20 +1,25 @@
 import React from "react";
 import about from "../../../images/about.jpg";
+import "./AboutMe.css";
 
 const AboutMe = () => {
   return (
-    <div className="h-[60vh] flex justify-between items-center lg:px-10 px-5 w-10/12 mx-auto">
-      <div className="w-5/12 flex justify-end mr-8">
-        <img className="w-8/12" src={about} alt="" />
+    <div className="lg:h-[60vh] h-screen flex lg:flex-row flex-col justify-between items-center lg:px-10 px-2 lg:w-10/12 w-11/12 mx-auto">
+      <div className="lg:w-5/12 w-10/12 flex  lg:justify-end justify-center mr-8 ">
+        <img
+          className="lg:w-9/12 w-full transform transition duration-500 cursor-pointer lg:mr-12 mr-0 about-image"
+          src={about}
+          alt=""
+        />
       </div>
-      <div className="w-7/12">
-        <h3 className="text-4xl font-bold">Who AM I</h3>
-        <p className="mt-4 font-semibold">
+      <div className="lg:w-7/12 w-11/12 lg:mb-0 mb-6">
+        <h3 className="lg:text-4xl text-2xl font-bold">Who AM I</h3>
+        <p className="mt-4 font-semibold lg:text-base text-xs">
           A Curious, Passionate Jr. MERN Stack Developer. Love to take Dare and
           handle it professionally. Handle difficulties and errors efficient
           way. Try to keep myself with new technologies and learn that.
         </p>
-        <button className="border border-yellow-600 px-6 py-2 hover:bg-yellow-600 text-black duration-500 lg:mt-4  text-white  transition w-30 mx-auto text-center rounded cursor-pointer">
+        <button className="border border-yellow-600 px-6 py-2 hover:bg-yellow-600 text-black duration-500 lg:mt-4 mt-6 transition w-30 mx-auto text-center rounded cursor-pointer">
           GIT HUB
         </button>
       </div>
