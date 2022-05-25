45367f2bb153c8cedeeb9b98e99aebb98448f3c7
NishatFariza <nishatfarzia78@gmail.com>
2022-05-25 08:34:40 +0600

    newsletter content style added

--- src/Pages/HomePage/AllReviews/Reviews/Reviews.jsx
+++ src/Pages/HomePage/AllReviews/Reviews/Reviews.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useQuery } from "react-query";
+import Loading from "../../../Shared/Loading/Loading";
 import Review from "../Rewiew/Review";
 
 const Reviews = () => {
@@ -12,15 +13,14 @@ const Reviews = () => {
   );
 
   if (isLoading) {
-    return <p>Loading....</p>;
+    return <Loading></Loading>;
   }
   return (
-    
-      <div className="bg-[url('https://i.ibb.co/5WmVLKK/review-bg.webp')] bg-cover">
-        <div className="lg:px-8">
+    <div className="bg-[url('https://i.ibb.co/5WmVLKK/review-bg.webp')] bg-cover">
+      <div className="lg:px-8">
         <div className="lg:w-6/12 md:w-8/12 sm:w-10/12 w-11/12 mx-auto text-center py-16">
           <h2 className="md:text-4xl text-2xl font-semibold mb-2 text-black">
-            OUR PRODUCTS
+            Our Clients
           </h2>
           <div className="h-1 w-24 rounded-lg bg-yellow-600 mx-auto"></div>
         </div>
--- src/Pages/HomePage/NewsLetter/NewsLatter.jsx
+++ src/Pages/HomePage/NewsLetter/NewsLatter.jsx
@@ -2,15 +2,18 @@ import React from "react";
 
 const NewsLatter = () => {
   return (
-    <div className="lg:mt-32 mt-10 mb-20">
-      <div className="bg-[url('https://i.ibb.co/Cwdn87T/newsletter-bg.webp')] bg-cover md:h-[70vh] h-[80vh] lg:w-11/12 mx-auto">
-        <div className="lg:w-8/12 w-10/12 mx-auto flex justify-center items-center">
+    <div className="lg:mt-32 mt-10 mb-20 ">
+      <div className="bg-[url('https://i.ibb.co/Cwdn87T/newsletter-bg.webp')] bg-cover md:h-[70vh] h-[80vh] lg:w-11/12 mx-auto rounded  flex justify-center items-center">
+        <div className="lg:w-8/12 w-10/12 mx-auto text-center">
           <div className="text-center">
-            <h4 className="primary-color lg:text-3xl md:text-2xl text-xl ">
-              SPECIAL OFFER FOR SUBSCRIPTION
+            <h4 className="primary-color lg:text-2xl md:text-xl text-xl lg:mb-3 mb-0">
+              SPECIAL <span className="text-yellow-400">OFFER</span> FOR
+              SUBSCRIPTION
             </h4>
-            <h3>GET INSTANT DISCOUNT FOR MEMBERSHIP</h3>
-            <p>
+            <h3 className="primary-color lg:text-3xl md:text-2xl text-xl ">
+              GET INSTANT DISCOUNT FOR MEMBERSHIP
+            </h3>
+            <p className="mb-6 primary-color lg:w-6/12 w-auto mx-auto mt-4">
               Subscribe our newsletter and all latest news of our latest
               product, promotion and offers
             </p>
@@ -18,9 +21,12 @@ const NewsLatter = () => {
               <input
                 type="text"
                 placeholder="Enter Your Email"
-                class="input input-bordered w-full max-w-xs"
+                class="input input-bordered w-full max-w-xs rounded-full"
               />
-              <input className="btn" value="Submit" />
+              <input
+                className="btn lg:w-2/12 w-full max-w-xs rounded-full -ml-28"
+                value="Submit"
+              />
             </div>
           </div>
         </div>
